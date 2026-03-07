696598545a4f368e7e55a16662e41fa6e8acb397
vtu26442-sketch <vtu26442@veltech.edu.in>
2026-03-07 16:25:26 +0530

    Time: 35 ms (51.53%), Space: 93.6 MB (47.62%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> maxDeque = new LinkedList<>();
+        Deque<Integer> minDeque = new LinkedList<>();
+        
+        int left = 0;
+        int result = 0;
+
+        for(int right = 0; right < nums.length; right++) {
+
+            while(!maxDeque.isEmpty() && nums[right] > maxDeque.peekLast())
+                maxDeque.pollLast();
+            maxDeque.addLast(nums[right]);
+
+            while(!minDeque.isEmpty() && nums[right] < minDeque.peekLast())
+                minDeque.pollLast();
+            minDeque.addLast(nums[right]);
+
+            while(maxDeque.peekFirst() - minDeque.peekFirst() > limit) {
+                if(nums[left] == maxDeque.peekFirst())
+                    maxDeque.pollFirst();
+
+                if(nums[left] == minDeque.peekFirst())
+                    minDeque.pollFirst();
+
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+        }
+
+        return result;
+    }
+}
